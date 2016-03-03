fd8f8d0b5d346557e29bab6b93ed3a0c16635c8f
basudez <basudez@gmail.com>
2016-03-03 09:58:01 +0100

    subiendo

---
+++ PrimeraAplicacionMaven/src/main/java/com/wpsnetwork/consola/App.java
@@ -0,0 +1,13 @@
+package com.wpsnetwork.consola;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
